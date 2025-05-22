55e59d23ec7b7119abf58aebd5cf8d4b5a09e652
mr-riad <riadgangni01302@gmail.com>
2025-05-22 21:21:10 +0600

    first commit

---
+++ lib/screens/dispose_lifecycle.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DisposeLifecycle extends StatefulWidget {
+  const DisposeLifecycle({super.key});
+
+  @override
+  State<DisposeLifecycle> createState() => _DisposeLifecycleState();
+}
+
+class _DisposeLifecycleState extends State<DisposeLifecycle> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
